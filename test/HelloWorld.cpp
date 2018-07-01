a3c0a6d19d66e03f4bbaa6adfabc91edf8707339
Aditya Aloy Sen <aloysen951@gmail.com>
2018-07-01 19:25:40 +0530

    the basic store in adysen lab linked from  @AloyASen -> memory engine

--- test/HelloWorld.cpp
+++
@@ -1,9 +0,0 @@
-#include "itkImage.h"
-#include <iostream>
-int main()
-{
-         using ImageType = itk::Image<unsigned short, 3>;
-        ImageType::Pointer image = ImageType::New();
-        std::cout<< "Itk hello world "<< std::endl;
-        return EXIT_SUCCESS;
-}
